9b1a761b1cbd5674724eb5936225b5097f96a8cf
Valeryia Berazhnaya <berezhnaya.designer@gmail.com>
2026-02-07 10:57:06 +0200

    Designing the Service Layer - Надо пересмореть всю логику и немного поправить. Сейчас много намешано, много лишнего

---
+++ ValeryiaBerazhnaya_Project/Service/Interfaces/IInventoryService.cs
@@ -0,0 +1,12 @@
+﻿using FurnitureWarehouse.Domain.Entities;
+
+namespace FurnitureWarehouse.Service.Interfaces
+{
+    public interface IInventoryService
+    {
+        Inventory LoadInventory();
+        IEnumerable<Furniture> GetAll();
+        IEnumerable<Furniture> SearchByName(string name);
+        IEnumerable<Furniture> SearchByCategory(string category);
+    }
+}
---
+++ ValeryiaBerazhnaya_Project/Service/InventoryService.cs
@@ -0,0 +1,51 @@
+﻿using FurnitureWarehouse.DataAccess.Interfaces;
+using FurnitureWarehouse.Domain.Entities;
+using FurnitureWarehouse.Domain.Enums;
+using FurnitureWarehouse.Domain.Services;
+using FurnitureWarehouse.Service.Interfaces;
+
+namespace FurnitureWarehouse.Service
+{
+    public class InventoryService : IInventoryService
+    {
+        private readonly Inventory _inventory;
+        private readonly FurnitureSearchService _searchService;
+
+        public InventoryService(IFurnitureRepository repository)
+        {
+            var loader = new InventoryLoader(repository);
+            _inventory = loader.Load();
+
+            _searchService = new FurnitureSearchService();
+        }
+
+        public Inventory LoadInventory()
+        {
+            return _inventory;
+        }
+
+        public IEnumerable<Furniture> GetAll()
+        {
+            return _inventory.Furnitures;
+        }
+
+        public IEnumerable<Furniture> SearchByName(string name)
+        {
+            return _searchService.SearchByName(
+                _inventory.Furnitures,
+                name
+            );
+        }
+
+        public IEnumerable<Furniture> SearchByCategory(string category)
+        {
+            if (!Enum.TryParse<FurnitureCategory>(category, true, out var parsed))
+                return Enumerable.Empty<Furniture>();
+
+            return _searchService.SearchByCategory(
+                _inventory.Furnitures,
+                parsed
+            );
+        }
+    }
+}
